63e0a81611e1daead3d1d7441c88579f142d9f8f
rabiga8 <rabiga.abdikerimova@gmail.com>
2024-03-09 21:55:49 -0500

    Issue#1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
 
-        System.out.printf("Welcome to COMP367!");
+        // Extract the hour of the day
+        int hour = currentTime.getHour();
 
+        // Get the student's name (replace "John" with the actual student's name)
+        String studentName = "John"; // Replace with actual student's name
+
+        // Determine the greeting message based on the time of day
+        String greetingMessage;
+        if (hour < 12) {
+            greetingMessage = "Good morning, " + studentName + ", Welcome to COMP367!";
+        } else {
+            greetingMessage = "Good afternoon, " + studentName + ", Welcome to COMP367!";
+        }
+
+        // Display the greeting message
+        System.out.println(greetingMessage);
     }
 }
